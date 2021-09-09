c6481f03d47bd2f2d18e0fc7e310363e4b19d72b
David <60575606+97060324@users.noreply.github.com>
2021-09-09 14:02:44 +0200

    Homepage changes

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,11 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="white"
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h1 style="color: black">De Dissel</h1>
       </div>
 
       <v-spacer></v-spacer>
@@ -32,29 +16,28 @@
         target="_blank"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2" style="color: black">Login</span>
       </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <deDisselLanding/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import deDisselLanding from './components/deDisselLanding';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    deDisselLanding,
   },
 
   data: () => ({
-    //
+    
   }),
 };
 </script>
---
+++ src/components/deDisselLanding.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <div class="col-lg-8 col-lg-offset-2 centered">
+      <div style="float: left; height: 50px;">
+        <h1 class="display-2 font-weight-bold mb-3">
+            De Dissel
+          </h1>
+
+          <p class="subheading font-weight-regular">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quae, odit, sapiente in reprehenderit, <br> 
+            est dolorum veniam ipsum veritatis itaque mollitia libero dicta dolore. Obcaecati pariatur  <br> 
+            dolor commodi, voluptates eveniet illo, cupiditate explicabo <br> 
+            asperiores reprehenderit adipisci animi! Odit sunt blanditiis cupiditate? Perspiciatis <br> 
+            odio magnam odit voluptatem in iusto amet eos illo laboriosam, <br> 
+          </p>
+      </div>
+      <div style="float: right; width: 200px; height: 50px;">
+        <v-img
+          lazy-src="https://picsum.photos/id/11/10/6"
+          max-height="300"
+          max-width="500"
+          src="https://picsum.photos/id/11/500/300"
+        ></v-img>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'deDisselLanding',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
