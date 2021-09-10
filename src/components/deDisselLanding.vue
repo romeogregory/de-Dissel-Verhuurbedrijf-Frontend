8ce6d42bf5402f646deaf3a605789394e599c692
David <60575606+97060324@users.noreply.github.com>
2021-09-10 15:11:25 +0200

    Homepage/landingpage done

--- src/components/deDisselLanding.vue
+++ src/components/deDisselLanding.vue
@@ -2,27 +2,16 @@
   <v-container>
     <div class="col-lg-8 col-lg-offset-2 centered">
       <div style="float: left; height: 50px;">
-        <h1 class="display-2 font-weight-bold mb-3">
-            De Dissel
-          </h1>
-
-          <p class="subheading font-weight-regular">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem quae, odit, sapiente in reprehenderit, <br> 
-            est dolorum veniam ipsum veritatis itaque mollitia libero dicta dolore. Obcaecati pariatur  <br> 
-            dolor commodi, voluptates eveniet illo, cupiditate explicabo <br> 
-            asperiores reprehenderit adipisci animi! Odit sunt blanditiis cupiditate? Perspiciatis <br> 
-            odio magnam odit voluptatem in iusto amet eos illo laboriosam, <br> 
-          </p>
-      </div>
-      <div style="float: right; width: 200px; height: 50px;">
-        <v-img
-          lazy-src="https://picsum.photos/id/11/10/6"
-          max-height="300"
-          max-width="500"
-          src="https://picsum.photos/id/11/500/300"
-        ></v-img>
+        <h1 class="display-2 font-weight-bold mb-3">De Dissel - Homepage</h1>
       </div>
     </div>
+    <v-btn style="margin-top: 200px;"
+        color="primary"
+        elevation="11"
+        large
+        x-large
+        x-small
+      >Bekijk alle caravans/campers</v-btn>
   </v-container>
 </template>
 
